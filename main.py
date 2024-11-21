6bb47f928f89b2b313eadd1e1b6cdcfe75be56b6
MakCimka <poduskapuska@gmail.com>
2024-11-21 10:34:33 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-with open ("text.txt", "r", encoding="utf-8")as file:
-    v = file.read()
-    e = v.replace("о", "a")
-with open ("output.txt", "w", encoding="utf-8")as file:
-    file.write(e)
+with open ("text.txt", "r", encoding="utf-8")as file: #Открываем файл
+    v = file.read() #Читаем его
+    e = v.replace("о", "a") 
+with open ("output.txt", "w", encoding="utf-8")as file:#Меняем о на а и переносим в другой файл
+    file.write(e) #Выводим 
